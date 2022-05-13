9d29b5cb225ac881484dd85291d1ec8739cb025a
MinhTuri <nguyenvanminh.vnu@gmail.com>
2022-05-13 16:45:51 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import nodejs from "@astrojs/node";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-import turbolinks from "@astrojs/turbolinks";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   adapter: nodejs(),
+  server: {
+    port: 3001,
+  },
 });

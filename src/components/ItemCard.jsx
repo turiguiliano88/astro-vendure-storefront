a74ecb2fb84d0ce5ebd61b5746e03b4ac0f8ad0b
MinhTuri <nguyenvanminh.vnu@gmail.com>
2022-06-21 13:03:28 +0200

    Add lazy loading to images

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -2,10 +2,11 @@ export default function ItemCard({ name, price, img_url, path }) {
   return (
     <div className="relative h-full overflow-hidden">
       <div className="h-full cursor-pointer">
-        <a href={path} className="h-full flex">
+        <a href={path} className="h-[200px] md:h-[300px] w-full flex">
           <img
+            loading="lazy"
             src={`${img_url}`}
-            className="object-cover hover:scale-105 transition delay-50"
+            className="object-cover hover:scale-105 transition delay-50 w-full"
           />
         </a>
       </div>
